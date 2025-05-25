bbcfba1e4ad5a5fc01e447ef66fc650fbcc9c72a
jiyun921 <ryn0921@naver.com>
2025-05-26 00:34:51 +0900

    로또 재귀(백트랙킹)으로 구현

--- Study2/3_Recursion/Q6603_lotto.py
+++ Study2/3_Recursion/Q6603_lotto.py
@@ -1,14 +1,27 @@
 import sys 
 input = sys.stdin.readline
-from itertools import combinations
 
-while (True):
-    S = list(map(int,input().split()))
-    k = S[0]
-    if k == 0:
-        break
-    S = S[1:]
-    pick = list(combinations(S, 6))
-    for i in pick :
-        print(*i)
-    print() 
+def pick_lotto(S, pick, S_idx):
+    if len(pick) == 6:
+        print(*pick)
+        
+    else: 
+        for i in range(S_idx, len(S)):
+            pick.append(S[i])    
+            pick_lotto(S, pick, i+1)
+            pick.pop()
+
+
+def solve():
+    while (True):
+        S = list(map(int,input().split()))
+        k = S[0]
+        if k == 0:
+            break
+        S = S[1:]
+
+        pick = [] 
+        pick_lotto(S, pick, 0)
+        print() 
+
+solve() 
