682c234ea1ad96ba021c91e962554bae056653c6
jiyun921 <ryn0921@naver.com>
2025-05-26 14:25:23 +0900

    Z 시간초과 해결

--- Study2/3_Recursion/Q1074_Z.py
+++ Study2/3_Recursion/Q1074_Z.py
@@ -16,11 +16,20 @@ def z_div(n, xy):
     
     else : 
         xy_list = [[0,0], [0,n//2], [n//2,0], [n//2,n//2]]
-
+				
+				# xy : 현재 블록의 좌상단 좌표 (전체 블록 기준 절대 좌표)
+        # offset : 현재 분할된 블록에서의 좌상단 좌표 (현재 블록 기준 상대 좌표)
+        # new_x, new_y : 새로운 블록의 좌상단 좌표 (전체 블록 기준 절대 좌표) 
+        
         for offset in xy_list:
             new_x = xy[0] + offset[0]
             new_y = xy[1] + offset[1]
-            z_div(n//2, [new_x,new_y])
+            if (new_x <= r < new_x + n//2 and new_y <= c < new_y + n//2):
+                z_div(n//2, [new_x,new_y])  
+                return 
+            else : 
+                order += (n//2) * (n//2)
+
 
 def solve():
     global r,c
