f1e194605b214a85fa344c94ad3d14007a7df8d7
jiyun921 <ryn0921@naver.com>
2025-08-05 17:03:38 +0900

    [fix]: BOJ1520 내리막길 - dp 적용해서 시간초과 해결

--- JavaAlgorithm/src/main/java/dfs/BOJ1520.java
+++ JavaAlgorithm/src/main/java/dfs/BOJ1520.java
@@ -5,11 +5,10 @@ import java.util.*;
 
 public class BOJ1520 {
     static int[][] graph;
-    static boolean[][] visited;
+    static int[][] dp_count;
     static int[] dx = {1,-1,0,0};
     static int[] dy = {0,0,-1,1};
     static int m,n;
-    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,35 +16,38 @@ public class BOJ1520 {
         m  = Integer.parseInt(st.nextToken());
         n  = Integer.parseInt(st.nextToken());
         graph = new int[m][n];
-        visited = new boolean[m][n];
-
+        dp_count = new int[m][n];
         for (int i=0; i<m ; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j=0; j<n; j++) {
                 graph[i][j] = Integer.parseInt(st.nextToken());
+                dp_count[i][j] = -1;
             }
         }
 
-        dfs(0,0);
-        System.out.println(count);
+        System.out.println(dfs(0,0));
 
     }
 
-    static void dfs(int x, int y) {
+    static int dfs(int x, int y) {
         if (x==m-1 && y==n-1) {
-            count++;
-            return;
+            return 1;
         }
 
-        visited[x][y] = true;
+        if (dp_count[x][y]!=-1) {
+            return dp_count[x][y];
+        }
+
+        dp_count[x][y] = 0;
+
         for (int i=0; i<4; i++){
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx>=0 && nx<m && ny>=0 && ny<n && !visited[nx][ny] && graph[nx][ny] < graph[x][y]) {
-                dfs(nx,ny);
+            if (nx>=0 && nx<m && ny>=0 && ny<n && graph[nx][ny] < graph[x][y]) {
+                dp_count[x][y] += dfs(nx,ny);
             }
         }
 
-        visited[x][y] = false;
+        return dp_count[x][y];
     }
 }
